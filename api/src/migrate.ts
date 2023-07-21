bcf1a058b7cf76e2ab4bdeda0d4852b1a65272d2
Suyog Waghere <suyogwaghere99@gmail.com>
2023-07-21 20:29:09 +0530

    API product model added

--- api/src/migrate.ts
+++ api/src/migrate.ts
@@ -6,7 +6,7 @@ export async function migrate(args: string[]) {
 
   const app = new PadmavatiApplication();
   await app.boot();
-  await app.migrateSchema({existingSchema, models:['User']});
+  await app.migrateSchema({existingSchema, models:['User','Product']});
 
   // Connectors usually keep a pool of opened connections,
   // this keeps the process running even after all work is done.
