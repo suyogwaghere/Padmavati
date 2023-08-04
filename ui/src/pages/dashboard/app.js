e44e040ea1a9b37f11325b31773f063885f8114b
Suyog Waghere <suyogwaghere99@gmail.com>
2023-08-04 19:35:08 +0530

    UI path after user login is updated
    Co-Authored-By: RoshanKolhe <58938030+RoshanKolhe@users.noreply.github.com>

--- ui/src/pages/dashboard/app.js
+++ ui/src/pages/dashboard/app.js
@@ -1,9 +1,11 @@
-import { lazy } from 'react';
 import { Helmet } from 'react-helmet-async';
+import { Navigate } from 'react-router-dom';
+import { paths } from 'src/routes/paths';
+
 // sections
 import { OverviewAppViewAdmin } from 'src/sections/overview/app/view';
 
-const ProductListPage = lazy(() => import('src/pages/product/list'));
+// const ProductListPage = lazy(() => import('src/pages/product/list'));
 // ----------------------------------------------------------------------
 
 export default function OverviewAppPage() {
@@ -14,8 +16,11 @@ export default function OverviewAppPage() {
       <Helmet>
         <title> Dashboard: App</title>
       </Helmet>
-
-      {userRole.includes('admin') ? <OverviewAppViewAdmin /> : <ProductListPage />}
+      {userRole.includes('admin') ? (
+        <OverviewAppViewAdmin />
+      ) : (
+        <Navigate to={paths.dashboard.product.root} replace />
+      )}
     </>
   );
 }
